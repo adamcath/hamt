db581918b762f69912c078ae3d4f45954afd1c0c
acath <acath@appdynamics.com>
2013-10-13 02:18:00 -0700

    First version of experimental hashed array-mapped trie

---
+++ src/HashedArrayMappedTrie.java
@@ -0,0 +1,320 @@
+import java.util.*;
+
+public class HashedArrayMappedTrie<K, V>
+{
+    public static class HashCollisionException extends RuntimeException
+    {
+    }
+
+    private final List<Entry<K, V>> rootEntries;
+
+    public HashedArrayMappedTrie()
+    {
+        this.rootEntries = createEmptyEntries();
+    }
+
+    private HashedArrayMappedTrie(List<Entry<K, V>> rootEntries)
+    {
+        this.rootEntries = rootEntries;
+    }
+
+    public V get(K key)
+    {
+        HashChunker hashIterator = new HashChunker(key.hashCode());
+        int firstHashChunk = hashIterator.next();
+        return rootEntries.get(firstHashChunk)
+                .lookupRecursively(hashIterator);
+    }
+
+    public HashedArrayMappedTrie<K, V> put(K key, V value)
+    {
+        HashChunker hashIterator = new HashChunker(key.hashCode());
+        int firstHashChunk = hashIterator.next();
+
+        Entry<K, V> oldEntry = rootEntries.get(firstHashChunk);
+        Entry<K, V> newEntry = oldEntry.putRecursively(hashIterator, value);
+
+        return new HashedArrayMappedTrie<K, V>(
+                copyAndSet(rootEntries, firstHashChunk, newEntry));
+    }
+
+    public int size()
+    {
+        int result = 0;
+        for (Entry<K, V> entry : rootEntries)
+            result += entry.size();
+        return result;
+    }
+
+    public String dump()
+    {
+        Indenter indenter = new Indenter();
+
+        StringBuilder result = new StringBuilder().append(indenter.get()).append("[\n");
+
+        indenter.increment();
+        for (int i = 0; i < rootEntries.size(); i++)
+            result.append(indenter.get())
+                    .append(String.format("%2x: ", i))
+                    .append(rootEntries.get(i).dump(indenter))
+                    .append("\n");
+        indenter.decrement();
+
+        result.append("]");
+
+        return result.toString();
+    }
+
+    static abstract class Entry<K, V>
+    {
+        public abstract V lookupRecursively(HashChunker keyHash);
+
+        public abstract Entry<K, V> putRecursively(HashChunker keyHash, V value);
+
+        public abstract String dump(Indenter indenter);
+
+        public abstract int size();
+    }
+
+    static class ZerotonEntry<K, V> extends Entry<K, V>
+    {
+        public V lookupRecursively(HashChunker keyHash)
+        {
+            return null;
+        }
+
+        public Entry<K, V> putRecursively(HashChunker keyHash, V value)
+        {
+            return new SingletonEntry<K, V>(keyHash.rest(), value);
+        }
+
+        public int size()
+        {
+            return 0;
+        }
+
+        public String dump(Indenter indenter)
+        {
+            return "-";
+        }
+    }
+
+    static class SingletonEntry<K, V> extends Entry<K, V>
+    {
+        private final int hashCodeSuffix;
+        private final V value;
+
+        private SingletonEntry(int hashCodeSuffix, V value)
+        {
+            this.hashCodeSuffix = hashCodeSuffix;
+            this.value = value;
+        }
+
+        public V lookupRecursively(HashChunker keyHash)
+        {
+            return hashCodeSuffix == keyHash.rest() ? value : null;
+        }
+
+        public Entry<K, V> putRecursively(HashChunker keyHash, V value)
+        {
+            if (keyHash.rest() == hashCodeSuffix)
+                throw new HashCollisionException();
+
+            return upgradeToMultiton().putRecursively(keyHash, value);
+        }
+
+        private Entry<K, V> upgradeToMultiton()
+        {
+            return new MultitonEntry<K, V>().putRecursively(new HashChunker(hashCodeSuffix), value);
+        }
+
+        public int size()
+        {
+            return 1;
+        }
+
+        public String dump(Indenter indenter)
+        {
+            return hashCodeSuffix + " => " + value.toString();
+        }
+    }
+
+    static class MultitonEntry<K, V> extends Entry<K, V>
+    {
+        private final boolean[] map;
+        private final List<Entry<K, V>> entries;
+
+        public MultitonEntry()
+        {
+            this(new boolean[32], new ArrayList<Entry<K, V>>(0));
+        }
+
+        private MultitonEntry(boolean[] map, List<Entry<K, V>> entries)
+        {
+            this.map = map;
+            this.entries = entries;
+        }
+
+        public V lookupRecursively(HashChunker keyHash)
+        {
+            int hashChunk = keyHash.next();
+            int entryIdx = hashToEntry(hashChunk);
+            if (entryIdx < 0)
+                return null;
+            else
+                return entries.get(entryIdx).lookupRecursively(keyHash);
+        }
+
+        public Entry<K, V> putRecursively(HashChunker keyHash, V value)
+        {
+            int hashChunk = keyHash.next();
+            int entryIdx = hashToEntry(hashChunk);
+            if (entryIdx < 0)
+            {
+                Entry<K, V> newEntry = new SingletonEntry<K, V>(keyHash.rest(), value);
+                return new MultitonEntry<K, V>(
+                        copyAndSet(map, hashChunk, true),
+                        copyAndInsert(entries, 0, newEntry));
+            }
+            else
+            {
+                Entry<K, V> oldEntry = entries.get(entryIdx);
+                Entry<K, V> newEntry = oldEntry.putRecursively(keyHash, value);
+                return new MultitonEntry<K, V>(
+                        map,
+                        copyAndSet(entries, entryIdx, newEntry));
+            }
+        }
+
+        private int hashToEntry(int hashChunk)
+        {
+            return map[hashChunk] ? countFlagsBelow(hashChunk, map) : -1;
+        }
+
+        public int size()
+        {
+            int result = 0;
+            for (Entry<K, V> entry : entries)
+                result += entry.size();
+            return result;
+        }
+
+        public String dump(Indenter indenter)
+        {
+            StringBuilder result = new StringBuilder().append("\n");
+            indenter.increment();
+            result.append(indenter.get()).append("[\n");
+            indenter.increment();
+            for (int i = 0; i < map.length; i++)
+            {
+                if (map[i])
+                {
+                    result.append(indenter.get())
+                            .append(String.format("%2x: ", i))
+                            .append(entries.get(hashToEntry(i)).dump(indenter))
+                            .append("\n");
+                }
+            }
+            indenter.decrement();
+            result.append(indenter.get()).append("]");
+            indenter.decrement();
+            return result.toString();
+        }
+    }
+
+    static class HashChunker
+    {
+        private static final int CHUNK_SIZE = 5;
+
+        private int hashCode;
+        private int bitsLeft = 32;
+
+        public HashChunker(int hashCode)
+        {
+            this.hashCode = hashCode;
+        }
+
+        public boolean hasNext()
+        {
+            return bitsLeft >= CHUNK_SIZE;
+        }
+
+        public int next()
+        {
+            int result = hashCode & 0x1f;
+            hashCode >>= CHUNK_SIZE;
+            bitsLeft -= CHUNK_SIZE;
+            return result;
+        }
+
+        public int rest()
+        {
+            return hashCode;
+        }
+    }
+
+    static class Indenter
+    {
+        private String indent = "";
+
+        public void increment()
+        {
+            indent += "  ";
+        }
+
+        public String get()
+        {
+            return indent;
+        }
+
+        public void decrement()
+        {
+            indent = indent.substring(2);
+        }
+    }
+
+    static int countFlagsBelow(int index, boolean[] bitmap)
+    {
+        assert index < bitmap.length;
+
+        int nFlagsOn = 0;
+        for (int i = 0; i < index; i++)
+            if (bitmap[i])
+                nFlagsOn++;
+        return nFlagsOn;
+    }
+
+    static <K, V> List<Entry<K, V>> copyAndSet(List<Entry<K, V>> entries, int index, Entry<K, V> newValue)
+    {
+        List<Entry<K, V>> newList = new ArrayList<Entry<K, V>>(entries.size());
+        for (int i = 0; i < entries.size(); i++)
+            newList.add(i == index ? newValue : entries.get(i));
+        return newList;
+    }
+
+    static <K, V> List<Entry<K, V>> copyAndInsert(List<Entry<K, V>> entries, int index, Entry<K, V> newValue)
+    {
+        List<Entry<K, V>> newList = new ArrayList<Entry<K, V>>(entries.size() + 1);
+        for (int i = 0; i < index; i++)
+            newList.add(entries.get(i));
+        newList.add(newValue);
+        for (int i = index; i < entries.size(); i++)
+            newList.add(entries.get(i));
+        return newList;
+    }
+
+    static boolean[] copyAndSet(boolean[] bitmask, int index, boolean newValue)
+    {
+        boolean[] newBitmask = Arrays.copyOf(bitmask, bitmask.length);
+        newBitmask[index] = newValue;
+        return newBitmask;
+    }
+
+    static <K, V> List<Entry<K, V>> createEmptyEntries()
+    {
+        List<Entry<K, V>> empty = new ArrayList<Entry<K, V>>(32);
+        for (int i = 0; i < 32; i++)
+            empty.add(new ZerotonEntry());
+        return empty;
+    }
+}
---
+++ src/HashedArrayMappedTrieTest.java
@@ -0,0 +1,110 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
+public class HashedArrayMappedTrieTest
+{
+
+    private static class MockString
+    {
+        private final String value;
+        private final int hashCode;
+
+        public MockString(String value, int hashCode)
+        {
+            this.value = value;
+            this.hashCode = hashCode;
+        }
+
+        public String toString()
+        {
+            return value;
+        }
+
+        public int hashCode()
+        {
+            return hashCode;
+        }
+    }
+
+    @Test
+    public void happyCase() throws Exception
+    {
+        assertEquals(
+                "bar",
+                new HashedArrayMappedTrie<String, String>()
+                        .put("foo", "bar")
+                        .get("foo"));
+
+        assertEquals(
+                "bar",
+                new HashedArrayMappedTrie<String, String>()
+                        .put("foo", "bar")
+                        .put("adam", "cath")
+                        .get("foo"));
+
+        assertEquals(
+                "cath",
+                new HashedArrayMappedTrie<String, String>()
+                        .put("foo", "bar")
+                        .put("adam", "cath")
+                        .get("adam"));
+    }
+
+    @Test
+    public void hashCodeCollision()
+    {
+        try
+        {
+            new HashedArrayMappedTrie<String, String>()
+                    .put("foo", "bar")
+                    .put("foo", "baz");
+            fail("Should have thrown");
+        } catch (HashedArrayMappedTrie.HashCollisionException e)
+        {
+            // Good, good
+        }
+    }
+
+    @Test
+    public void afterModificationOldVersionIsUnchanged()
+    {
+        HashedArrayMappedTrie<String, String> v1 = new HashedArrayMappedTrie<String, String>();
+        String v1dump = v1.dump();
+
+        HashedArrayMappedTrie<String, String> v2 = v1.put("adam", "cath");
+        String v2dump = v2.dump();
+        assertEquals(v1dump, v1.dump());
+        assertEquals(0, v1.size());
+
+        HashedArrayMappedTrie<String, String> v3 = v2.put("stephanie", "greer");
+        assertEquals(v2dump, v2.dump());
+        assertEquals(1, v2.size());
+        assertEquals(v1dump, v1.dump());
+        assertEquals(0, v1.size());
+        assertEquals(2, v3.size());
+    }
+
+    @Test
+    public void testCountFlagsBelow() throws Exception
+    {
+
+        class Fixture
+        {
+            void doTest(boolean[] bitmap, int index, int expected)
+            {
+                assertEquals(expected, HashedArrayMappedTrie.countFlagsBelow(index, bitmap));
+            }
+        }
+
+        Fixture f = new Fixture();
+
+        f.doTest(new boolean[]{true}, 0, 0);
+        f.doTest(new boolean[]{false}, 0, 0);
+        f.doTest(new boolean[]{true, true}, 1, 1);
+        f.doTest(new boolean[]{false, true}, 1, 0);
+        f.doTest(new boolean[]{false, false}, 1, 0);
+        f.doTest(new boolean[]{true, true, false}, 2, 2);
+    }
+}
